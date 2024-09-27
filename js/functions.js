95d8047a5fca108706648ce3aaffad8d2381203f
nastya-pon-04 <nastyaponomera20041030@gmail.com>
2024-09-27 18:26:08 +0500

    Без консоли

--- js/functions.js
+++ js/functions.js
@@ -2,21 +2,23 @@ function checkTheString (string, lenght){
   return string.length <= lenght;
 }
 
-console.log(checkTheString('проверяемая строка', 20));
-console.log(checkTheString('проверяемая строка', 18));
-console.log(checkTheString('проверяемая строка', 10));
+checkTheString('Сегоднятепло', 10);
+// console.log(checkTheString('проверяемая строка', 20));
+// console.log(checkTheString('проверяемая строка', 18));
+// console.log(checkTheString('проверяемая строка', 10));
 
 function thePalindrom (palindromString){
-  let string = palindromString.replaceAll(' ','').toUpperCase();
-  for (let i =0; i < string.length / 2; i++){
-    if (string.at(i) !== string.at(-i -1)){
+  const string = palindromString.replaceAll(' ','').toUpperCase();
+  for (let i = 0; i < string.length / 2; i++){
+    if (string.at(i) !== string.at(-i - 1)){
       return false;
     }
   }
   return true;
 }
 
-console.log(thePalindrom('топот'));
-console.log(thePalindrom('ДовОд'));
-console.log(thePalindrom('Кекс'));
-console.log(thePalindrom('Лёша на полке клопа нашёл '));
+thePalindrom('шалаш');
+// console.log(thePalindrom('топот'));
+// console.log(thePalindrom('ДовОд'));
+// console.log(thePalindrom('Кекс'));
+// console.log(thePalindrom('Лёша на полке клопа нашёл '));
