e5167499afc4798249060862eef3d2423033d9a0
nastya-pon-04 <nastyaponomera20041030@gmail.com>
2024-10-24 17:09:46 +0500

    Разделила на модули

---
+++ js/util.js
@@ -0,0 +1,8 @@
+// Модуль с вспомогательными функциями.
+// Функция getRandomInt возвращает случайное число. Думаю, ее можно считать вспомогательной.
+
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getRandomInt();
